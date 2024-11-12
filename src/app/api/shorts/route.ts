04bd208db3e7ac5ed069a1257f8f26939d595a61
estebangf <esteban.fernandez93@outlook.com.ar>
2024-11-12 09:11:27 -0300

    Se comentó la función de GET en la api por ahora hasta encontrar el problema que se genera a producción.

--- src/app/api/shorts/route.ts
+++ src/app/api/shorts/route.ts
@@ -45,59 +45,59 @@ export async function POST (req: Request) {
 
 
 // GET ALL
-export const GET = auth(async function GET (req) {
+// export const GET = auth(async function GET (req) {
 
-  console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
-  console.log("req", req)
-  console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
-  console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
-  console.log("req.auth", req.auth)
-  console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+//   console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+//   console.log("req", req)
+//   console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+//   console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+//   console.log("req.auth", req.auth)
+//   console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
 
-  const session = req.auth;
+//   const session = req.auth;
 
-  if (!session || !session.user || !session.user.id) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-  }
+//   if (!session || !session.user || !session.user.id) {
+//     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+//   }
 
-  // const userId = session.user.id; // ID único del usuario autenticado
-  const userId = session.user.email; // Email único del usuario autenticado
+//   // const userId = session.user.id; // ID único del usuario autenticado
+//   const userId = session.user.email; // Email único del usuario autenticado
 
-  console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
-  console.log("userId", userId)
-  console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+//   console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+//   console.log("userId", userId)
+//   console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
 
-  if (!userId) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-  }
+//   if (!userId) {
+//     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+//   }
 
-  const db = await connectToDatabase();
+//   const db = await connectToDatabase();
 
-  // Obtener todas las URLs con sus estadísticas
-  const shorts = await db.collection<Short>(SHORTS_COLLECTION_NAME).find({ userId })
-    .sort({ creationDate: -1 })
-    .toArray();
+//   // Obtener todas las URLs con sus estadísticas
+//   const shorts = await db.collection<Short>(SHORTS_COLLECTION_NAME).find({ userId })
+//     .sort({ creationDate: -1 })
+//     .toArray();
 
-  // Transformar los datos para incluir `lastAccessed` calculado de la colección `clicks`
-  const enhancedUrls = await Promise.all(
-    shorts.map(async (url) => {
-      const lastAccessedEntry = await db.collection('clicks')
-        .find({ shortId: url.shortId })
-        .sort({ accessedAt: -1 })
-        .limit(1)
-        .toArray();
+//   // Transformar los datos para incluir `lastAccessed` calculado de la colección `clicks`
+//   const enhancedUrls = await Promise.all(
+//     shorts.map(async (url) => {
+//       const lastAccessedEntry = await db.collection('clicks')
+//         .find({ shortId: url.shortId })
+//         .sort({ accessedAt: -1 })
+//         .limit(1)
+//         .toArray();
 
-      return {
-        userId: url.userId,
-        shortId: url.shortId,
-        title: url.title,
-        originalUrl: url.originalUrl,
-        clickCount: url.clickCount,
-        creationDate: url.creationDate,
-        lastAccessed: lastAccessedEntry[0]?.accessedAt || null,
-      };
-    })
-  );
+//       return {
+//         userId: url.userId,
+//         shortId: url.shortId,
+//         title: url.title,
+//         originalUrl: url.originalUrl,
+//         clickCount: url.clickCount,
+//         creationDate: url.creationDate,
+//         lastAccessed: lastAccessedEntry[0]?.accessedAt || null,
+//       };
+//     })
+//   );
 
-  return NextResponse.json(enhancedUrls);
-})
+//   return NextResponse.json(enhancedUrls);
+// })
